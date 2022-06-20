987384a5ac8e6cd30484afb465c1f709fdb6cc1a
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-20 13:57:18 -0300

    feat(User): add user attributes and configure attributes swagger definitions

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: "Receive one name from the user",
+    example: "PedroAlcantara"
+  })
+  @IsString()
+  userName: string;
+
+  @ApiProperty({
+    description: "Receive one email from the user",
+    example: "email@email.com"
+  })
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    description: "Receive one encryption of the password. The user sends the password.",
+    example: "5f4dcc3b5aa765d61d8327deb882cf99"
+  })
+  @IsString()
+  password: string;
+}
