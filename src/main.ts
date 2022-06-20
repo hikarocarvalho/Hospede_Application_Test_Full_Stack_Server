e4dfd3e2b9862d82a86de0405353b5415be743e8
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-20 13:54:16 -0300

    feat(App): configuration of cors, swagger, and server port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Minha Lista')
+    .setDescription('This is the Api to the my list system')
+    .setVersion('1.0')
+    .addTag('Minha Lista')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api',app, document);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
