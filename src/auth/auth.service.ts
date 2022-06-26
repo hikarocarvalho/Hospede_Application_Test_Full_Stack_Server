1b42e5cbc610d90b0376fe47feafd23c7bb85e27
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:11:25 -0300

    feat(Auth): create auth route

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthResponseDto } from './dto/auth-response.dto';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService,) {}
+
+    async login(authDto: AuthDto): Promise<AuthResponseDto> {
+        const { email, password } = authDto;
+        
+        const user = await this.prisma.dbUsers.findUnique({ where: {email}})
+        
+        if(!user) {
+            throw new NotFoundException('Invalid user or password!')
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if (!isPasswordValid){
+            throw new UnauthorizedException('Invalid user');
+        }
+
+        delete user.password;
+
+        return {
+            token: this.jwtService.sign({ email }),
+            user,
+        };
+    }
+}
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class AuthDto{
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'User Email.',
+        example: 'email@email.com'
+    })
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        description: 'User Password.',
+        example: 'wefasdf@gsa_56'
+    })
+    password: string;
+}
