72ff10c882afe2ae6ae21f5a1d3bb86c76dfe669
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:14:14 -0300

    feat(UserDto): add new validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({
     description: "Receive one name from the user",
     example: "PedroAlcantara"
   })
+  @IsNotEmpty()
   @IsString()
   userName: string;
 
@@ -13,13 +14,15 @@ export class CreateUserDto {
     description: "Receive one email from the user",
     example: "email@email.com"
   })
+  @IsNotEmpty()
   @IsString()
   email: string;
 
   @ApiProperty({
-    description: "Receive one encryption of the password. The user sends the password.",
-    example: "5f4dcc3b5aa765d61d8327deb882cf99"
+    description: 'User Password.',
+    example: 'wefasdf@gsa_56'
   })
+  @IsNotEmpty()
   @IsString()
   password: string;
 }
