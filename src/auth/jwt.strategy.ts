3ba1abee1d6ce24a89fa1a1666a438b0d524b79b
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:11:55 -0300

    feat(Auth): create jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { email: string }) {
+    const user = await this.prisma.dbUsers.findUnique({
+      where: { email: payload.email },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Not authorized user');
+    }
+
+    delete user.password;
+
+    return user;
+  }
+}
